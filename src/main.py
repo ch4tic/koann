226b2f3423827a9207b09864e75fdb6516074350
Eman Ćatić <belusic012@gmail.com>
2021-11-09 21:29:58 +0100

    prepravke

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main():
     #putanja = input("Unesi putanju do slike: ")
     #while putanja == "":
     #    putanja = input("Unesi putanju do slike: ")
-
+    config = ('-l eng --oem 1 --psm 3')
     slika = cv2.imread('receipt.jpg', cv2.IMREAD_COLOR)
     #cv2.imshow('Original', slika)
     tekst = pytesseract.image_to_string(slika, config=config)
