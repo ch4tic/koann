134245c3b4d80b09817b29aa518bbabe035c8f0a
Eman Catic <belusic012@gmail.com>
2021-11-20 10:53:57 +0100

    skaliranje slika na 300 DPI

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-# Authori: Eman Ćatić i Rijad Gadžo
 # Datum: -
-# TODO: dodati unos korisnika za nove slike, upload na Google Drive? 
+# TODO: SPEED OPTIMISATION 
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,24 +11,19 @@ from PIL import Image, ImageEnhance
 
 def main():
     config = ('-l bos --oem 1 --psm 3') # config sa tesseract
-
     if len(sys.argv) < 2:
         print("sintaksa: python3 main.py ../img/ ime_slike")
         sys.exit(1)
 
     putanja = sys.argv[1]
-    os.chdir(putanja) # program mijenja folder u folder koji je unio korisnik
+    os.chdir(putanja)
     ime_slike = sys.argv[2]
-    slika = Image.open(putanja + ime_slike) # ucitavanje slike korisnika
+    slika = cv2.imread(putanja+ime_slike)
+    slika = cv2.resize(slika, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
 
-    kontrast = ImageEnhance.Contrast(slika)
-    kontrast.enhance(2).save(putanja+"novaslika.jpg") # povecanje kontrasta 2x i sacuvanje poboljsane slike pod imenom 'novaslika.jpg'
-    svjetlina = ImageEnhance.Brightness(slika)
-    svjetlina.enhance(2).save(putanja+"novaslika.jpg") # povecanje svjetline 2x i sacuvanje poboljsane slike pod imenom 'novaslika.jpg'
-    slika = cv2.imread(putanja+"novaslika.jpg", cv2.IMREAD_COLOR) # ucitavanje poboljsane slike
-    tekst = pytesseract.image_to_string(slika, config=config) # prepoznavanje teksta na slici
-    print(tekst) # ispisivanje prepoznatog teksta sa slike
-
+    slika = cv2.imread(putanja+ime_slike, cv2.IMREAD_COLOR)
+    tekst = pytesseract.image_to_string(slika, config=config)
+    print(tekst)
 
 if __name__ == "__main__":
     main()
