93fbeaa3ff0f4983925d700f7a28977a0bf3001f
Eman Catic <belusic012@gmail.com>
2021-11-20 17:30:55 +0100

    efikasnije procesuiranje fotografija, preciznost skripte bolja

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # Datum: -
-# TODO: SPEED OPTIMISATION 
+# TODO: folder organizacija 
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,8 +20,14 @@ def main():
     ime_slike = sys.argv[2]
     slika = cv2.imread(putanja+ime_slike)
     slika = cv2.resize(slika, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-
-    slika = cv2.imread(putanja+ime_slike, cv2.IMREAD_COLOR)
+    slika = cv2.cvtColor(slika, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    slika = cv2.dilate(slika, kernel, iterations=1)
+    slika = cv2.erode(slika, kernel, iterations=1)
+    cv2.threshold(cv2.medianBlur(slika, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    cv2.adaptiveThreshold(cv2.medianBlur(slika, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    
+    slika = cv2.imread(putanja+ime_slike, cv2.COLOR_BGR2GRAY)
     tekst = pytesseract.image_to_string(slika, config=config)
     print(tekst)
 
