d428e48a96377fce32434d148459f2567b9ee14e
Eman Catic <belusic012@gmail.com>
2021-11-20 21:30:53 +0100

    dodani komentari

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+# Authori: Eman Ćatić i Rijad Gadžo
 # Datum: -
-# TODO: folder organizacija 
+# TODO:  
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,36 +12,51 @@ import cv2
 import os
 from PIL import Image, ImageEnhance
 
-def main():
-    config = ('-l bos --oem 1 --psm 3') # config sa tesseract
+def main(): 
+    # -- KONFIGURACIJA -- 
+    config = ('-l bos --oem 1 --psm 3')
     if len(sys.argv) < 2:
         print("sintaksa: python3 main.py ../img/ ime_slike")
-        sys.exit(1)
-    timestr = time.strftime("%Y%m%d-%H%M%S")
+        sys.exit(1) 
 
-    putanja = sys.argv[1]
-    os.chdir(putanja)
+    # -- VARIJABLE -- 
+    filename = "output.txt" # ime output file-a
+    timestr = time.strftime("%Y%m%d-%H%M%S") # format imena foldera
+    # postavljanje putanje i imena slike
+    putanja = sys.argv[1] 
     ime_slike = sys.argv[2]
-    slika = cv2.imread(putanja+ime_slike)
-    slika = cv2.resize(slika, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-    slika = cv2.cvtColor(slika, cv2.COLOR_BGR2GRAY)
+    os.chdir(putanja) # promjena foldera u unesenu putanju
+    fputanja = putanja+ime_slike
+
+    # -- IMAGE PROCESSING -- 
+    slika = cv2.imread(fputanja) # ucitavanje slike
+    # skaliranje slike
+    slika = cv2.resize(slika, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC) 
+    # pretvaranje slike u grayscale
+    slika = cv2.cvtColor(slika, cv2.COLOR_BGR2GRAY) 
+
+    # blurrovanje slika 
     kernel = np.ones((1, 1), np.uint8)
     slika = cv2.dilate(slika, kernel, iterations=1)
     slika = cv2.erode(slika, kernel, iterations=1)
     cv2.threshold(cv2.medianBlur(slika, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     cv2.adaptiveThreshold(cv2.medianBlur(slika, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
     
-    fputanja = putanja+ime_slike
-    slika = cv2.imread(putanja+ime_slike, cv2.COLOR_BGR2GRAY)
+    # ucitavanje poboljsane slike 
+    slika = cv2.imread(fputanja, cv2.COLOR_BGR2GRAY) 
+    # detekcija teksta iz slike i ispisivanje istog
     tekst = pytesseract.image_to_string(slika, config=config)
-    # print(tekst)
-    os.chdir("../arhiva/")
-    os.mkdir(timestr)
-    filename = "output.txt"
-    file = open(filename, "w+")
-    file.write(tekst)
-    file.close()
-    shutil.move("output.txt", timestr)
+    print(tekst)
+
+    # -- FILE ORGANIZACIJA --
+    os.chdir("../arhiva/") # promjena foldera u '/arhiva/'
+    os.mkdir(timestr) # pravljenje novog foldera
+    file = open(filename, "w+") # pravljenje output filea
+    file.write(tekst) # ispisivanje detektovanog teksta u file
+    file.close() # zatvaranje filea
+    # premjestanje koristene slike i output filea u novi folder
+    shutil.move(filename, timestr)
     shutil.move(fputanja, timestr)
+
 if __name__ == "__main__":
     main()
